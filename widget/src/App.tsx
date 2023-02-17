7394c3ead42f214fc681b6c1f1859778e259fca3
Gabriel Brother <gabriel.fe.mello@gmail.com>
2023-02-17 15:21:12 -0300

    text changed

--- widget/src/App.tsx
+++ widget/src/App.tsx
@@ -32,9 +32,9 @@ function App() {
     <div className={`App${miss ? '-miss' :''}`}>
 
       {miss ? <div className={"App-header-gratefull"}>
-        <p>Agradecemos sua avaliação!</p>
+        <p>Obrigado 🧡<br/>Temos um presente surpresa para você, <a href="https://gifs.eco.br/wp-content/uploads/2021/07/lindos-gifs-de-flores-0.gif" target="_blank">clique aqui</a> para ver.</p>
       </div> : <header className={"App-header"}>
-        <p>Numa escala de 1 a 10, quanto você indicaria a Tiflux para alguém?</p>
+        <p>Numa escala de 1 a 10, quanto você indicaria a Tiflux para um amigo ou conhecido?</p>
         
         <div className="optionsCheck">
           {radioOptions.map((option:any)=>
