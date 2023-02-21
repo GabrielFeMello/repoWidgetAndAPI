05440e455cc748b56e7dd566fa21b22c66ce1859
Gabriel Brother <gabriel.fe.mello@gmail.com>
2023-02-20 23:10:30 -0300

    official public

--- api/dist/index.js
+++ api/dist/index.js
@@ -4,5 +4,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-app_1.default.listen(3333);
+app_1.default.listen(8080);
 //# sourceMappingURL=index.js.map
