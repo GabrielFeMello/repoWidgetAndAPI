5d15b0e0f0c478de68edd9e92ed86d6ed2c449c3
Gabriel Brother <gabriel.fe.mello@gmail.com>
2023-02-21 02:38:51 -0300

    baseUrl

--- widget/src/util/baseURL.ts
+++ widget/src/util/baseURL.ts
@@ -2,4 +2,4 @@ console.log("NODE_ENV", process.env.NODE_ENV)
 export const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3333' 
-    : process.env.NODE_ENV === 'test' ? 'https://repo-widget-and-api-8184.vercel.app/' :`${window.location.origin}:3333`
+    : process.env.NODE_ENV === 'test' ? 'https://repo-widget-and-api-8184.vercel.app' :`https://repo-widget-and-api-8184.vercel.app`
